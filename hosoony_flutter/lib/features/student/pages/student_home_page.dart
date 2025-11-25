f1d5845de550ec764c4247cbce47780c31f280c0
Thaka International <106166601+Thaka-International@users.noreply.github.com>
2025-11-25 23:58:00 +0300

    fix: إضافة import لـ ErrorHandler

--- hosoony_flutter/lib/features/student/pages/student_home_page.dart
+++ hosoony_flutter/lib/features/student/pages/student_home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import '../../../core/theme/tokens.dart';
+import '../../../core/errors/error_handler.dart';
 import '../../../services/api_service.dart';
 import '../../../services/auth_service.dart';
 import '../../../shared_ui/widgets/copyright_widget.dart';
